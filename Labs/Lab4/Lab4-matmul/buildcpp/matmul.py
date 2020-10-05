1892a48d8f2d1edecfeecb4d208c60638ce23611
mduschenes <mduschen@umich.edu>
2020-10-05 00:04:07 -0400

    new py file

---
+++ Labs/Lab4/Lab4-matmul/buildcpp/matmul.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+import os,sys,time
+import numpy as np
+
+
+# Matrix Operations
+def timer(quiet=False):
+	def wrapper(func):
+		def _wrapper(*args,**kwargs):
+			time0 = time.time()
+			output = func(*args,**kwargs)
+			deltatime = time.time()-time0
+			if not quiet:
+				print('Time: %0.5f s'%(deltatime))
+			return output
+		return _wrapper
+	return wrapper
+
+
+@timer(quiet=True)
+def generate_random_matrix(m,n,quiet=True,label=None):
+	# return np.random.rand(m,n)
+	arr = np.random.randint(1,10,(m,n)).astype(float)
+	if not quiet:
+		print_matrix(arr,label)
+	return arr
+
+
+def print_matrix(arr,label=None):
+	if label is not None:
+		print(label)
+		print(arr)
+		print()
+	return
+
+
+@timer(quiet=False)
+def dgemm(A,B,C,alpha,beta):
+	C *= beta
+	C += alpha*np.dot(A,B)
+	return
+
+
+# Arguments
+N = 10
+
+
+Nargs = len(sys.argv)-1
+if Nargs > 0:
+	N = int(sys.argv[1])
+
+print('N = %d'%(N))
+
+
+# Initialize Matrices
+arrs = {}
+keys = ['A','B','C']
+for k in keys:
+	arrs[k] = generate_random_matrix(N,N,quiet=False,label=k)
+
+
+
+# Multiply Matrices
+
+params = {'alpha':1.0,'beta':0.0}
+
+dgemm(**arrs,**params)
+
+print_matrix(arrs['C'],'New C')
+
+
+
+
+
+
